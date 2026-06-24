e6fd556a4ea4bf52b45e33519d21f6a1852739f2
Haseeb Ali <haseebtariq963100@gmail.com>
2026-06-24 18:45:45 +0500

    feat: setup monorepo and frontend foundation

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,7 @@
+import { APP_NAME } from "@/shared/constants/app";
+
+function App() {
+  return <h1>{APP_NAME}</h1>;
+}
+
+export default App;
---
+++ apps/web/src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+function LoginPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold text-blue-600">
+                Tailwind Working
+            </h1>
+        </div>
+    );
+}
+
+export default LoginPage;
